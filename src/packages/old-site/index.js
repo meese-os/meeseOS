d8b414b098bac8fe53e73dd9af786be211c50e13
Aaron Meese <ajmeese7@gmail.com>
2022-02-05 06:05:37 -0500

    Working old-site
    The terminal also still works here.

--- src/packages/old-site/index.js
+++ src/packages/old-site/index.js
@@ -1,39 +1,39 @@
-import './index.scss';
-import osjs from 'osjs';
-import {name as applicationName} from './metadata.json';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/App';
+import "./index.scss";
+import osjs from "osjs";
+import {name as applicationName} from "./metadata.json";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./src/App";
 
 // Our launcher
 const register = (core, args, options, metadata) => {
 	// Create a new Application instance
-	const proc = core.make('osjs/application', {args, options, metadata});
+	const proc = core.make("osjs/application", {args, options, metadata});
 
 	// Create a new Window instance
 	const win = proc.createWindow({
-		// TODO: Show title instead of name at the top of the window somehow
-    id: proc.metadata.name,
-    title: metadata.title,
+		// TODO: Show title instead of name in app list, like the Music Player
+		id: proc.metadata.name,
+		title: metadata.title,
 		dimension: {width: 700, height: 450},
 		position: {left: 300, top: 200}
 	});
 
 	win.maximize();
-	win.on('destroy', () => proc.destroy())
+	win.on("destroy", () => proc.destroy())
 	win.render($content => ReactDOM.render(React.createElement(App), $content));
 
 	// Creates a new WebSocket connection (see server.js)
-	//const sock = proc.socket('/socket');
-	//sock.on('message', (...args) => console.log(args))
-	//sock.on('open', () => sock.send('Ping'));
+	//const sock = proc.socket("/socket");
+	//sock.on("message", (...args) => console.log(args))
+	//sock.on("open", () => sock.send("Ping"));
 
 	// Use the internally core bound websocket
-	//proc.on('ws:message', (...args) => console.log(args))
-	//proc.send('Ping')
+	//proc.on("ws:message", (...args) => console.log(args))
+	//proc.send("Ping")
 
 	// Creates a HTTP call (see server.js)
-	//proc.request('/test', {method: 'post'})
+	//proc.request("/test", {method: "post"})
 	//.then(response => console.log(response));
 
 	return proc;
--- src/packages/old-site/src/App.js
+++ src/packages/old-site/src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
 import {
-  showNavigationbar,
+	showNavigationbar,
 } from "./editable-stuff/configurations.json";
 
 import MainBody from "./components/home/MainBody";
@@ -15,22 +15,22 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 const Home = () => {
-  return (
-    <Fragment>
-      <MainBody />
-      <AboutMe />
-      <Articles />
-      <Project />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<MainBody />
+			<AboutMe />
+			<Articles />
+			<Project />
+		</Fragment>
+	);
 };
 
 const App = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
-    {showNavigationbar && <Navbar />}
-    <Route path="/" exact component={Home} />
-    <Footer />
-  </BrowserRouter>
+	<BrowserRouter basename={process.env.PUBLIC_URL + "/"}>
+		{showNavigationbar && <Navbar />}
+		<Route path="/" exact component={Home} />
+		<Footer />
+	</BrowserRouter>
 );
 
 export default App;
