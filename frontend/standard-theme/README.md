a917104c6c2b1ec2952789227736c5a6a14b61f5
Aaron Meese <ajmeese7@gmail.com>
2022-02-13 16:40:53 -0500

    Standard theme package

---
+++ frontend/standard-theme/README.md
@@ -0,0 +1,16 @@
+<p align="center">
+  <img alt="OS.js Logo" src="https://raw.githubusercontent.com/os-js/gfx/master/logo-big.png" />
+</p>
+
+[OS.js](https://www.os-js.org/) is an [open-source](https://raw.githubusercontent.com/os-js/OS.js/master/LICENSE) web desktop platform with a window manager, application APIs, GUI toolkit, filesystem abstractions and much more.
+
+# OS.js v3 Default Theme
+
+This is the Default Theme module for OS.js v3
+
+## Installation
+
+```bash
+npm install @osjs/standard-theme
+npm run package:discover
+```
---
+++ frontend/standard-theme/webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const minimize = mode === 'production';
+const plugins = [];
+
+if (mode === 'production') {
+  plugins.push(new OptimizeCSSAssetsPlugin({
+    cssProcessorOptions: {
+      discardComments: true
+    },
+  }));
+}
+
+module.exports = {
+  mode,
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'index.js'),
+  ],
+  optimization: {
+    minimize,
+  },
+  externals: {
+    osjs: 'OSjs'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    ...plugins
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(svg|png|jpe?g|gif|webp)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name(file) {
+                return file.replace(/\\/g, '/').match(/src\/icons/) ? 'icons/[name].[ext]' : '[hash].[ext]';
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
