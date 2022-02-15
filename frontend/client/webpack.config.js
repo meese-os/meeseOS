cc947b96d2e98489dde250c35b9d8896ce0c39d4
Aaron Meese <ajmeese7@gmail.com>
2022-02-14 20:19:44 -0500

    Client ~> Webpack 5

--- frontend/client/webpack.config.js
+++ frontend/client/webpack.config.js
@@ -1,82 +1,75 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const mode = process.env.NODE_ENV || 'development';
-const minimize = mode === 'production';
+const mode = process.env.NODE_ENV || "development";
+const minimize = mode === "production";
 const plugins = [];
 
-if (mode === 'production') {
-  plugins.push(new OptimizeCSSAssetsPlugin({
-    cssProcessorOptions: {
-      discardComments: true,
-      map: {
-        inline: false
-      }
-    },
-  }));
+if (mode === "production") {
+	plugins.push(new CssMinimizerPlugin({
+		minimizerOptions: {
+			preset: [ "advanced" ],
+		},
+	}));
 }
 
 module.exports = {
-  mode,
-  devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'index.js'),
-  ],
-  output: {
-    library: 'osjsClient',
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-    sourceMapFilename: '[file].map',
-    filename: '[name].js'
-  },
-  optimization: {
-    minimize,
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    ...plugins
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(svg|png|jpe?g|gif|webp)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: 'file-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)\/(?!@osjs)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+	mode,
+	devtool: "source-map",
+	entry: [
+		path.resolve(__dirname, "index.js"),
+	],
+	output: {
+		library: "osjsClient",
+		libraryTarget: "umd",
+		umdNamedDefine: true,
+		sourceMapFilename: "[file].map",
+		filename: "[name].js"
+	},
+	optimization: {
+		minimize,
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		}),
+		...plugins
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(svg|png|jpe?g|gif|webp)$/,
+				type: "asset/resource",
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules\/(?!@osjs)/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
 };
