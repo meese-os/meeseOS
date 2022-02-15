3d34bb86bac5a103d698f1753540b3a44928bea3
Aaron Meese <ajmeese7@gmail.com>
2022-02-14 19:52:10 -0500

    Standard theme ~> Webpack 5

--- frontend/standard-theme/webpack.config.js
+++ frontend/standard-theme/webpack.config.js
@@ -1,80 +1,77 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const mode = process.env.NODE_ENV || 'development';
-const minimize = mode === 'production';
+const mode = process.env.NODE_ENV || "development";
+const minimize = mode === "production";
 const plugins = [];
 
-if (mode === 'production') {
-  plugins.push(new OptimizeCSSAssetsPlugin({
-    cssProcessorOptions: {
-      discardComments: true
-    },
-  }));
+if (mode === "production") {
+	plugins.push(new CssMinimizerPlugin({
+		minimizerOptions: {
+			preset: [ "advanced" ],
+		},
+	}));
 }
 
 module.exports = {
-  mode,
-  devtool: 'source-map',
-  entry: [
-    path.resolve(__dirname, 'index.js'),
-  ],
-  optimization: {
-    minimize,
-  },
-  externals: {
-    osjs: 'OSjs'
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-    ...plugins
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(svg|png|jpe?g|gif|webp)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name(file) {
-                return file.replace(/\\/g, '/').match(/src\/icons/) ? 'icons/[name].[ext]' : '[hash].[ext]';
-              }
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+	mode,
+	devtool: "source-map",
+	entry: [
+		path.resolve(__dirname, "index.js"),
+	],
+	optimization: {
+		minimize,
+	},
+	externals: {
+		osjs: "OSjs"
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css"
+		}),
+		...plugins
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(svg|png|jpe?g|gif|webp)$/,
+				type: "asset/resource",
+				exclude: /node_modules/,
+				generator: {
+					filename: file => {
+						return file.replace(/\\/g, "/")
+							.match(/src\/icons/) ? "icons/[name].[ext]" : "[hash].[ext]";
+					}
+				},
+			},
+			{
+				test: /\.(sa|sc|c)ss$/,
+				exclude: /node_modules/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							sourceMap: true
+						}
+					},
+					{
+						loader: "sass-loader",
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			}
+		]
+	}
 };
