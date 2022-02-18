1727b07ae5a43f2eb1501217ca8291faf60a7b21
Aaron Meese <ajmeese7@gmail.com>
2022-02-17 20:45:04 -0500

    Gnome icons @Webpack 5

---
+++ frontend/gnome-icons/README.md
@@ -0,0 +1,18 @@
+<p align="center">
+  <img alt="OS.js Logo" src="https://raw.githubusercontent.com/os-js/gfx/master/logo-big.png" />
+</p>
+
+[OS.js](https://www.os-js.org/) is an [open-source](https://raw.githubusercontent.com/os-js/OS.js/master/LICENSE) web desktop platform with a window manager, application APIs, GUI toolkit, filesystem abstractions and much more.
+
+# aaronmeese.com Gnome Icon Theme
+
+This is the Gnome Icon Theme module for aaronmeese.com
+
+[![Build Status](https://travis-ci.org/os-js/osjs-gnome-icons.svg?branch=master)](https://travis-ci.org/os-js/osjs-gnome-icons)
+
+## Installation
+
+```bash
+npm install @aaronmeese.com/gnome-icons
+npm run package:discover
+```
--- frontend/gnome-icons/webpack.config.js
+++ frontend/gnome-icons/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
-const mode = process.env.NODE_ENV || "development";
-const minimize = mode === "production";
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const minimize = mode === "production";
 const plugins = [];
 
 if (mode === "production") {
@@ -23,15 +23,7 @@ module.exports = {
 	optimization: {
 		minimize,
 	},
-	externals: {
-		osjs: "OSjs"
-	},
 	plugins: [
-		new CopyWebpackPlugin({
-			patterns: [
-				"logo.svg"
-			]
-		}),
 		new MiniCssExtractPlugin({
 			filename: "[name].css",
 			chunkFilename: "[id].css"
@@ -43,7 +35,13 @@ module.exports = {
 			{
 				test: /\.(svg|png|jpe?g|gif|webp)$/,
 				type: "asset/resource",
-				exclude: /node_modules/
+				exclude: /node_modules/,
+				generator: {
+					filename: file => {
+						return file.replace(/\\/g, "/")
+							.match(/src\/icons/) ? "icons/[name].[ext]" : "[hash].[ext]";
+					}
+				},
 			},
 			{
 				test: /\.(sa|sc|c)ss$/,
