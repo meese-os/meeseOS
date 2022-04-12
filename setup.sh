08905a984f1976a557f7ee1fde1211ded6f09447
Aaron Meese <ajmeese7@gmail.com>
2022-04-11 21:14:21 -0400

    Attempting setup script
    Going to see if I can use this to easily automate my new Jenkins pipeline.

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install nvm for Node installation versioning
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Attempt to make nvm available to shell:
+# https://stackoverflow.com/a/23757895/6456163
+export NVM_DIR="$HOME/.nvm"
+. $NVM_DIR/nvm.sh;
+
+# Install Node 16.x
+nvm install 16
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install rush packages and build
+sudo npm install -g rush
+rush install
+rush build
+
+# Set up the terminal app
+sh ./apps/terminal/scripts/setup.sh
+
+# Run the server
+cd website
+npm run package:discover
+npm run serve
