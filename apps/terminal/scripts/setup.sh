64c9fe4431c5c1f3def21705d7662d1edfad28f3
Aaron Meese <ajmeese7@gmail.com>
2022-04-12 21:42:58 -0400

    Pushing for debugging purposes

--- apps/terminal/scripts/setup.sh
+++ apps/terminal/scripts/setup.sh
@@ -7,12 +7,10 @@
 
 # Adds the `.env` variables to the shell environment:
 # https://gist.github.com/mihow/9c7f559807069a03e302605691f85572?permalink_comment_id=2706921#gistcomment-2706921
-if [ -f .env ]
-then
+if [ -f .env ]; then
 	# Exports the .env variables to the shell environment:
 	export $(sed 's/#.*//g' .env | xargs)
-elif [ -z "${PASSWORD}"]
-then
+elif [ -z "${PASSWORD}" ]; then
 	# If no `.env` file exists and the env vars aren't already set,
 	# ask the user for the data:
 	read -p "Username: " USERNAME
@@ -26,14 +24,21 @@ else
 	export PASSWORD
 fi
 
+# TODO: FIX THE COMMANDS BELOW
+# TODO: SEE WHY GIT IS NOT WORKING IN VS CODE
+
+echo "PWD: `pwd`"
+echo "LS -la: `ls -la`"
+echo "Parent ls -la: `ls -la ..`"
+
 # Will create the user only if they do not already exist
-sh create-user.sh
+sh -x ./create-user.sh
 
 # Installs some helpful packages used by the app
 sudo apt-get install -y sshpass python2 build-essential
 
 # Optional: Install `oh-my-posh`
-sh oh-my-posh.sh
+sh -x ./oh-my-posh.sh
 
 # Clean up the environment
 echo "Cleaning up..."
