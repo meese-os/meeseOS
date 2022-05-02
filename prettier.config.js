1640f6ecebef6a81cbeefa34ab34c7d2663c2513
Aaron Meese <ajmeese7@gmail.com>
2022-05-01 20:20:33 -0400

    Updated prettier configuration
    Since `.editorconfig` was being ignored by DeepSource even though it is supposed to be respected by Prettier, I have specified exactly what I want so I stop getting low-quality pull requests.

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,15 @@
 module.exports = {
+  useTabs: true,
+  tabWidth: 2,
   singleQuote: false,
   jsxSingleQuote: false,
   trailingComma: "es5",
+  overrides: [
+    {
+      files: ["*.json", "prettier.config.js"],
+      options: {
+        useTabs: false
+      }
+    },
+  ]
 };
