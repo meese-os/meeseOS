88c35f623eb23ba037e66bc4dbe509a4cdb71b46
Aaron Meese <ajmeese7@gmail.com>
2022-05-15 16:35:30 -0400

    Improved clean slate script
    The find command was giving issues for the `.gnupg` file in the user's home directory and wasn't happy about deleting a user that was still in use/owned any processes. The script now uses sudo to find files and forcefully deletes the user regardless of their current status.

--- apps/terminal/scripts/clean-slate.sh
+++ apps/terminal/scripts/clean-slate.sh
@@ -5,14 +5,14 @@
 ##
 
 # Set all the user's .bashrc files to no longer be immutable
-find /jail/home -type f -name '.bashrc' -exec sudo chattr -i {} +
+sudo find /jail/home -type f -name '.bashrc' -exec sudo chattr -i {} +
 
 # Delete all the users to ensure there are no artifacts in the system
 # https://stackoverflow.com/a/2108296/6456163
 for dir in /jail/home/*/
 do
 	dir=${dir%*/}
-	sudo userdel "${dir##*/}"
+	sudo userdel -f "${dir##*/}"
 done
 
 # Delete the jail
