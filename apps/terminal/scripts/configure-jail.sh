81283faad0797690d738a8e0fff34f8b92e69061
Aaron Meese <ajmeese7@gmail.com>
2022-05-16 19:31:10 -0400

    Tightened directory permissions

--- apps/terminal/scripts/configure-jail.sh
+++ apps/terminal/scripts/configure-jail.sh
@@ -14,6 +14,7 @@ declare -a commands=(
 	"/usr/bin/clear"
 	"/usr/bin/which"
 	"/usr/bin/tr"      # Required for oh-my-posh
+	"/bin/rm"          # Required for oh-my-posh
 )
 
 # Props to https://stackoverflow.com/a/22432604/6456163 for array looping code
@@ -49,3 +50,7 @@ sudo mkdir /jail/proc
 sudo mount -t proc /proc /jail/proc/
 # TODO: Try remounting /proc on every reboot with
 # https://serverfault.com/a/302124/537331
+
+
+# Create a `/tmp` directory for use by oh-my-posh
+sudo mkdir /jail/tmp
