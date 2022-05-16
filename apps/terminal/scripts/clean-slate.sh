e983ceddb376375c0bfeb76f1215c03365991f41
Aaron Meese <ajmeese7@gmail.com>
2022-05-16 19:02:01 -0400

    Unmount /jail/proc in clean slate
    The script was having difficulties because it was trying to delete the content of the real `/proc` directory when purging the jail.

--- apps/terminal/scripts/clean-slate.sh
+++ apps/terminal/scripts/clean-slate.sh
@@ -7,6 +7,9 @@
 # Set all the user's .bashrc files to no longer be immutable
 sudo find /jail/home -type f -name '.bashrc' -exec sudo chattr -i {} +
 
+# Unmount's the /jail/proc filesystem
+sudo umount /jail/proc
+
 # Delete all the users to ensure there are no artifacts in the system
 # https://stackoverflow.com/a/2108296/6456163
 for dir in /jail/home/*/
